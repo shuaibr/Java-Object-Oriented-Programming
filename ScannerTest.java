70c94b93a87bdc58f7dbefd31f34ed83366f17bf
shuaibr <31935226+shuaibr@users.noreply.github.com>
2020-05-29 19:23:20 -0400

    simple print test:

---
+++ ScannerTest.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ScannerTest {
+    public static void main(String args[]) {
+        System.out.println("Enter a series of integers. Press 'q' to quit.");
+        // Read from the keyboard.
+        Scanner s = new Scanner(System.in);
+        int result = 0;
+
+        while (s.hasNext()) {
+        	
+        }
+       
+
+        s.close();
+        System.out.println("The total is " + result);
+    }
+}
